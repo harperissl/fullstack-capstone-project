564f0e1d28fb066513034f63c224b4d1a2cab7c0
harperissl <harperissl@users.noreply.github.com>
2024-03-28 16:10:46 +0800

    Update app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -51,8 +51,8 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-    res.send("Inside the server")
-})
+    res.send("Inside the server");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
