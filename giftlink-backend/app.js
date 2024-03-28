626b47ec4aaab3abf960687d12dc19a1d54ddba2
harperissl <harperissl@gmail.com>
2024-03-28 01:47:31 -0400

    Gift API Task

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -5,11 +5,11 @@ const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
+const { loadData } = require("./util/import-mongo/index");
 
 
 const app = express();
-app.use("*",cors());
+app.use("*", cors());
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
@@ -24,7 +24,7 @@ app.use(express.json());
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 //{{insert code here}}
-
+const giftRoutes = require('./routes/giftRoutes');
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
 
@@ -37,7 +37,7 @@ app.use(pinoHttp({ logger }));
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 //{{insert code here}}
-
+app.use('/api/gifts', giftRoutes);
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
 
@@ -48,7 +48,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Inside the server")
 })
 
--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -2,15 +2,15 @@ router.get('/', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
         // const db = {{insert code here}}
-
+        const db = await connectToDatabase();
         // Task 2: use the collection() method to retrieve the gift collection
         // {{insert code here}}
-
+        const collection = db.collection("gifts");
         // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
         // const gifts = {{insert code here}}
-
+        const gifts = await collection.find({}).toArray();
         // Task 4: return the gifts using the res.json method
-        res.json(/* {{insert code here}} */);
+        res.json(gifts);
     } catch (e) {
         console.error('Error fetching gifts:', e);
         res.status(500).send('Error fetching gifts');
@@ -21,15 +21,15 @@ router.get('/:id', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
         // const db = {{insert code here}}
-
+        const db = await connectToDatabase();
         // Task 2: use the collection() method to retrieve the gift collection
         // {{insert code here}}
-
+        const collection = db.collection("gifts");
         const id = req.params.id;
 
         // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
         // {{insert code here}}
-
+        const gift = await collection.findOne({ id: id });
         if (!gift) {
             return res.status(404).send('Gift not found');
         }
