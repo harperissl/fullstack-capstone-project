e3181d1bd4434fc0c77118c67478fccd01e6fae4
harperissl <harperissl@users.noreply.github.com>
2024-03-28 15:51:08 +0800

    Implement Back-end in Register Component

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,4 +1,12 @@
 import React, { useState } from 'react';
+//Step 1 - Task 1
+import {urlConfig} from '../../config';
+
+//Step 1 - Task 2
+import { useAppContext } from '../../context/AuthContext';
+
+//Step 1 - Task 3
+import { useNavigate } from 'react-router-dom';
 
 import './RegisterPage.css';
 
@@ -8,75 +16,119 @@ function RegisterPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    //Step 1 - Task 4
+     const [showerr, setShowerr] = useState('');
+
+    //Step 1 - Task 5
+    const navigate = useNavigate();
+    const { setIsLoggedIn } = useAppContext();
+
     const handleRegister = async () => {
-        console.log("Register invoked")
+        const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
+            //Step 1 - Task 6
+            method: 'POST',
+            //Step 1 - Task 7
+            headers: {
+                'content-type': 'application/json',
+            },
+            //Step 1 - Task 8
+            body: JSON.stringify({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password
+            })
+        });
+
+        //Step 2 - Task 1
+        const json = await response.json();
+        console.log('json data', json);
+        console.log('er', json.error);
+
+        //Step 2 - Task 2
+        if (json.authtoken) {
+            sessionStorage.setItem('auth-token', json.authtoken);
+            sessionStorage.setItem('name', firstName);
+            sessionStorage.setItem('email', json.email);
+        //Step 2 - Task 3
+            setIsLoggedIn(true);
+        //Step 2 - Task 4
+            navigate('/app');
+        }
+        if (json.error) {
+        //Step 2 - Task 5
+            setShowerr(json.error);
+        }
     }
 
-return (
-    <div className="container mt-5">
-        <div className="row justify-content-center">
-            <div className="col-md-6 col-lg-4">
-                <div className="register-card p-4 border rounded">
-                    <h2 className="text-center mb-4 font-weight-bold">Register</h2>
-                    <div className="mb-3">
-                        <label htmlFor="firstName" className="form-label">FirstName</label>
-                        <input
-                            id="firstName"
-                            type="text"
-                            className="form-control"
-                            placeholder="Enter your firstName"
-                            value={firstName}
-                            onChange={(e) => setFirstName(e.target.value)}
-                        />
-                    </div>
+    return (
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-6 col-lg-4">
+                    <div className="register-card p-4 border rounded">
+                        <h2 className="text-center mb-4 font-weight-bold">Register</h2>
+                        <div className="mb-3">
+                            <label htmlFor="firstName" className="form-label">FirstName</label>
+                            <input
+                                id="firstName"
+                                type="text"
+                                className="form-control"
+                                placeholder="Enter your firstName"
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
+                            />
+                        </div>
 
-                    {/* last name */}
+                        {/* last name */}
 
-                    <div className="mb-3">
-                        <label htmlFor="lastName" className="form-label">LastName</label>
-                        <input
-                            id="lastName"
-                            type="text"
-                            className="form-control"
-                            placeholder="Enter your lastName"
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
-                        />
-                    </div>
+                        <div className="mb-3">
+                            <label htmlFor="lastName" className="form-label">LastName</label>
+                            <input
+                                id="lastName"
+                                type="text"
+                                className="form-control"
+                                placeholder="Enter your lastName"
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
+                            />
+                        </div>
 
-                    {/* email  */}
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email</label>
-                        <input
-                            id="email"
-                            type="text"
-                            className="form-control"
-                            placeholder="Enter your email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
+                        {/* email  */}
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input
+                                id="email"
+                                type="text"
+                                className="form-control"
+                                placeholder="Enter your email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        {/* Step 2 - Task 6*/}
 
-                    <div className="mb-4">
-                        <label htmlFor="password" className="form-label">Password</label>
-                        <input
-                            id="password"
-                            type="password"
-                            className="form-control"
-                            placeholder="Enter your password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
+                                <div className="text-danger">{showerr}</div>
+                        </div>
+
+                        <div className="mb-4">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input
+                                id="password"
+                                type="password"
+                                className="form-control"
+                                placeholder="Enter your password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <button className="btn btn-primary w-100 mb-3" onClick={handleRegister}>Register</button>
+                        <p className="mt-4 text-center">
+                            Already a member? <a href="/app/login" className="text-primary">Login</a>
+                        </p>
                     </div>
-                    <button className="btn btn-primary w-100 mb-3" onClick={handleRegister}>Register</button>
-                    <p className="mt-4 text-center">
-                        Already a member? <a href="/app/login" className="text-primary">Login</a>
-                    </p>
                 </div>
             </div>
         </div>
-    </div>
-);
+    );
 }
 
 export default RegisterPage;
