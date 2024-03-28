290a997195df5b0f76a63d59f22f015a6ea6a065
harperissl <harperissl@users.noreply.github.com>
2024-03-28 15:54:59 +0800

    Implement Back-end in Login Component

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,35 +1,39 @@
 const express = require('express');
-const app = express();
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { body, validationResult } = require('express-validator');
 const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');  // Import Pino logger
+dotenv.config();
 
 const logger = pino();  // Create a Pino logger instance
 
+//Create JWT secret
 dotenv.config();
-
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
     try {
-        // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
-        const db = await connectToDatabase();
+      //Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
+      const db = await connectToDatabase();
 
-        // Task 2: Access MongoDB collection
-        const collection = db.collection("users");
+      //Access the `users` collection
+      const collection = db.collection("users");
 
-        //Task 3: Check for existing email
-        const existingEmail = await collection.findOne({ email: req.body.email });
+      //Check for existing email in DB
+      const existingEmail = await collection.findOne({ email: req.body.email });
+
+        if (existingEmail) {
+            logger.error('Email id already exists');
+            return res.status(400).json({ error: 'Email id already exists' });
+        }
 
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
-        const email = req.body.email;
+        const email=req.body.email;
 
-        //Task 4: Save user details in database
+        //Save user details
         const newUser = await collection.insertOne({
             email: req.body.email,
             firstName: req.body.firstName,
@@ -44,12 +48,52 @@ router.post('/register', async (req, res) => {
             },
         };
 
+        //Create JWT
         const authtoken = jwt.sign(payload, JWT_SECRET);
         logger.info('User registered successfully');
-        res.json({authtoken,email});
+        res.json({ authtoken,email });
     } catch (e) {
-         return res.status(500).send('Internal server error');
+        logger.error(e);
+        return res.status(500).send('Internal server error');
     }
+});
+
+    //Login Endpoint
+router.post('/login', async (req, res) => {
+    console.log("\n\n Inside login")
+
+    try {
+        // const collection = await connectToDatabase();
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+        const theUser = await collection.findOne({ email: req.body.email });
+
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            if(!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong pasword' });
+            }
+            let payload = {
+                user: {
+                    id: theUser._id.toString(),
+                },
+            };
+
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+
+            const authtoken = jwt.sign(payload, JWT_SECRET);
+            logger.info('User logged in successfully');
+            return res.status(200).json({ authtoken, userName, userEmail });
+        } else {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).json({ error: 'Internal server error', details: e.message });
+      }
 });
 
 module.exports = router;
--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,14 +1,71 @@
 import React, { useState,useEffect } from 'react';
+//Step 1 - Task 1
+import {urlConfig} from '../../config';
+//Step 1 - Task 2
+import { useAppContext } from '../../context/AuthContext';
+//Step 1 - Task 3
+import { useNavigate } from 'react-router-dom';
 
 import './LoginPage.css';
 
 function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    //Step 1 - Task 4
+    const [incorrect, setIncorrect] = useState('');
+    //Step 1 - Task 5
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    //Step 1 - Task 6
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+          navigate('/app')
+        }
+      }, [navigate])
 
     const handleLogin = async (e) => {
         e.preventDefault();
-}
+        //api call
+        const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+            //Step 1 - Task 7
+            method: 'POST',
+            //Step 1 - Task 8
+          headers: {
+            'content-type': 'application/json',
+            'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+          },
+        //Step 1 - Task 9
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          })
+        });
+
+        //Step 2: Task 1
+        const json = await res.json();
+        console.log('Json',json);
+        if (json.authtoken) {
+            //Step 2: Task 2
+          sessionStorage.setItem('auth-token', json.authtoken);
+          sessionStorage.setItem('name', json.userName);
+          sessionStorage.setItem('email', json.userEmail);
+            //Step 2: Task 3
+          setIsLoggedIn(true);
+            //Step 2: Task 4
+          navigate('/app');
+        } else {
+            //Step 2: Task 5
+          document.getElementById("email").value="";
+          document.getElementById("password").value="";
+          setIncorrect("Wrong password. Try again.");
+          setTimeout(() => {
+            setIncorrect("");
+          }, 2000);
+        }
+
+      }
 
 
     return (
@@ -25,7 +82,7 @@ function LoginPage() {
                                 className="form-control"
                                 placeholder="Enter your email"
                                 value={email}
-                                onChange={(e) => setEmail(e.target.value)}
+                                onChange={(e) => {setEmail(e.target.value); setIncorrect("")}}
                             />
                         </div>
                         <div className="mb-4">
@@ -36,10 +93,12 @@ function LoginPage() {
                                 className="form-control"
                                 placeholder="Enter your password"
                                 value={password}
-                                onChange={(e) => setPassword(e.target.value)}
+                                onChange={(e) => {setPassword(e.target.value);setIncorrect("")}}
                             />
+
+                            {/*Step 2: Task 6*/}
+                            <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
                         </div>
-                        {/* Include appropriate error message if login is incorrect*/}
                         <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
                         <p className="mt-4 text-center">
                             New here? <a href="/app/register" className="text-primary">Register Here</a>
