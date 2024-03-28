ee330550751a41264d88f74c98e5ff97080e848b
harperissl <harperissl@users.noreply.github.com>
2024-03-28 15:57:56 +0800

    Implement Back-end in Profile Component

--- giftlink-frontend/src/components/Profile/Profile.js
+++ giftlink-frontend/src/components/Profile/Profile.js
@@ -65,15 +65,19 @@ const handleSubmit = async (e) => {
 
     const payload = { ...updatedDetails };
     const response = await fetch(`${urlConfig.backendUrl}/api/auth/update`, {
-      //Step 1: Task 1
-      //Step 1: Task 2
-      //Step 1: Task 3
+      method: "PUT",//Step 1: Task 1
+      headers: {//Step 1: Task 2
+        "Authorization": `Bearer ${authtoken}`,
+        "Content-Type": "application/json",
+        "Email": email,
+      },
+      body: JSON.stringify(payload),//Step 1: Task 3
     });
 
     if (response.ok) {
       // Update the user details in session storage
-      //Step 1: Task 4
-      //Step 1: Task 5
+      setUserName(updatedDetails.name);//Step 1: Task 4
+      sessionStorage.setItem("name", updatedDetails.name);//Step 1: Task 5
       setUserDetails(updatedDetails);
       setEditMode(false);
       // Display success message to the user
